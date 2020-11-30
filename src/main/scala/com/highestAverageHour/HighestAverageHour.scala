3152f96323fc9f2712d9a47dd015f5638281f80c
cheluvesha <cheluvesh.b@gmail.com>
2020-12-01 02:35:00 +0530

    [CHELUVESHA]Added: Comments

--- src/main/scala/com/highestAverageHour/HighestAverageHour.scala
+++ src/main/scala/com/highestAverageHour/HighestAverageHour.scala
@@ -1,6 +1,9 @@
 package com.highestAverageHour
 
 
+/***
+ * Dependency Used Hadoop - client to perform MapReduce
+ */
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{IntWritable, Text}
@@ -8,6 +11,11 @@ import org.apache.hadoop.mapreduce.Job
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 
+/***
+ * Map Reduce Driver Class
+ * Declares the Mapper and Reducer class
+ * Declares and Initializes File Input and Output Path
+ */
 object HighestAverageHour {
 
   def main(args: Array[String]): Unit = {
