af2af67718ca6fe0d125815f257d335aa5f9a195
cheluvesha <cheluvesh.b@gmail.com>
2020-12-01 02:39:37 +0530

    [CHELUVESHA]Added: Test case for Mapper & reduce

--- src/test/scala/com/userlogAnalysisTest/HighestNoOfTimeLateTest.scala
+++ src/test/scala/com/userlogAnalysisTest/HighestNoOfTimeLateTest.scala
@@ -7,15 +7,18 @@ import org.junit.runner.RunWith
 import org.powermock.modules.junit4.PowerMockRunner
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
-
-
+/***
+ * Test Class for LowestAverageHour
+ * class verifies mapper and reduce class methods
+ * Dependencies used Junit PowerMocker, Mockito-all-api, MRUnit and Scala Test
+ */
 @RunWith(classOf[PowerMockRunner])
 class HighestNoOfTimeLateTest extends FunSuite with BeforeAndAfter {
 
-  val input = "2019-09-18 08:30:01,2,62.72,894.14,50.0,2,266662,0,276411,0.03,0.31,0.25,7896846336,921227264,5305417728,1514033152,646832128,354349056,1315852288,105070592,6553718784,30149586944,19135401984,9459044352,106413,4166,1267769344,321496064,43248,71000,464244,298865,2450592,2549,3840,0,0,0,0,0:08:34.669398,iamnzm@outlook.com,44.0,45.0,python,6"
+  val input = "2019-09-18 09:30:01,2,62.72,894.14,50.0,2,266662,0,276411,0.03,0.31,0.25,7896846336,921227264,5305417728,1514033152,646832128,354349056,1315852288,105070592,6553718784,30149586944,19135401984,9459044352,106413,4166,1267769344,321496064,43248,71000,464244,298865,2450592,2549,3840,0,0,0,0,0:08:34.669398,iamnzm@outlook.com,44.0,45.0,python,6"
   val userNameWithDate = "iamnzm@outlook.com,2019-09-18"
-  val userDataKey: String = "iamnzm@outlook.com,2019-09-18,08:30:01"
-  val time = "08:30:01"
+  val userDataKey: String = "iamnzm@outlook.com,2019-09-18,09:30:01"
+  val time = "09:30:01"
   var reducer: HighestNoOfTimeComingLateReducer = _
   var mapper: HighestNoOfTimeComingLateMapper = _
   var mapDriver: MapDriver[Object, Text, Text, Text] = _
@@ -39,10 +42,17 @@ class HighestNoOfTimeLateTest extends FunSuite with BeforeAndAfter {
   before {
     setup()
   }
+
   test("givenDataMapperHasToPassValueOneWhenMouseAndKeyboardHitsMoreThanZero") {
     mapDriver.withInput(new LongWritable(),new Text(input))
-    mapDriver.withOutput(new Text(userDataKey),new Text("08:30:01"))
+    mapDriver.withOutput(new Text(userDataKey),new Text(time))
     mapDriver.runTest()
+  }
+
+  test("givenDataItMustCheckWhetherItsLateTimeOrEarly") {
+    mapReduceDriver.addInput(new LongWritable(), new Text(input))
+    mapReduceDriver.addOutput(new Text("iamnzm@outlook.com"), new Text("Late By "+1+" times"))
+    mapReduceDriver.runTest()
   }
 
 }
