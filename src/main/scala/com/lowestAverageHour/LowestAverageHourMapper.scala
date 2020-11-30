22b8af8668d65cfe5a0e9610be07791666a37325
cheluvesha <cheluvesh.b@gmail.com>
2020-12-01 00:23:58 +0530

    [CHELUVESHA]Refactored: HardCoded into Variables

--- src/main/scala/com/lowestAverageHour/LowestAverageHourMapper.scala
+++ src/main/scala/com/lowestAverageHour/LowestAverageHourMapper.scala
@@ -10,7 +10,7 @@ import org.apache.hadoop.mapreduce.Mapper
 class LowestAverageHourMapper extends Mapper[Object,Text,Text,IntWritable] {
   val outputKey = new Text()
   val one = 1
-
+  val zero = 0
   /***
    * Mapper method reads data and redirects to Reduce class as Key and Value Pair
    * @param key Object
@@ -20,18 +20,18 @@ class LowestAverageHourMapper extends Mapper[Object,Text,Text,IntWritable] {
   override
   def map(key: Object, value: Text, context: Mapper[Object, Text, Text, IntWritable]#Context): Unit = {
     val userLogData = value.toString.split(",")
-    val dateTime = userLogData(0)
-    val date = dateTime.substring(0,dateTime.indexOf(' '))
+    val dateTime = userLogData(zero)
+    val date = dateTime.substring(zero,dateTime.indexOf(' '))
     val time =  dateTime.substring(dateTime.indexOf(' '))
     val keyBoardHits: Double = userLogData(41).toDouble
     val mouseClicks: Double = userLogData(42).toDouble
     val engineerID: String = userLogData(40)
     outputKey.set(engineerID+":"+date+":"+time)
-    var num = 0
-    if((keyBoardHits >= 0) && (mouseClicks > 0)) {
+    var num = zero
+    if((keyBoardHits >= zero) && (mouseClicks > zero)) {
       num = one
     }
-    else if ((keyBoardHits > 0) && (mouseClicks >= 0)) {
+    else if ((keyBoardHits > zero) && (mouseClicks >= zero)) {
       num = one
     }
     context.write(outputKey,new IntWritable(num))
