66eb40e80312a8413e882fdf8cb6e7e2744090ad
cheluvesha <cheluvesh.b@gmail.com>
2020-11-30 23:55:32 +0530

    [CHELUVESHA]Refactored: HardCoded Values and Comments

--- src/main/scala/com/highestAverageHour/HighestAverageHourReducer.scala
+++ src/main/scala/com/highestAverageHour/HighestAverageHourReducer.scala
@@ -8,22 +8,45 @@ import scala.collection.immutable.ListMap
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
+/***
+ * Reducer class aggregates values wrt customized key and calculates lowest average hour
+ */
 class HighestAverageHourReducer extends Reducer[Text,IntWritable,Text,Text] {
   var map: mutable.LinkedHashMap[String, ListBuffer[Int]] = _
   var avgTimeMap: mutable.LinkedHashMap[String, Double] = _
   var days = 0
+  var one = 0
+  var zero = 0
+  var idleLimits = 0
+  var minutes = 0
 
+  /***
+   * setup method initializes instance variables before reduce() method executes
+   * @param context - Reducer[Text, IntWritable, Text, Text]#Context
+   */
   override def setup(context: Reducer[Text, IntWritable, Text, Text]#Context): Unit = {
+    // mutable.LinkedHashMap[String, ListBuffer[Int] - Customized UserData Key and Value List
     map = new mutable.LinkedHashMap[String, ListBuffer[Int]]()
+    // mutable.LinkedHashMap[String, Double] - Stores UserID as Key and Hour as Value
     avgTimeMap = new mutable.LinkedHashMap[String, Double]
     days = 6
+    one = 1
+    zero = 0
+    idleLimits = 6
+    minutes = 5
   }
 
+  /***
+   * reduce() accepts Key and Value From Mapper class and Process the User Data as Defined
+   * @param key Text
+   * @param values lang.Iterable[IntWritable]
+   * @param context Reducer[Text, IntWritable, Text, Text]#Context
+   */
   override
   def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, Text]#Context): Unit = {
     val keyData = key.toString.split(":")
-    val keyName = keyData(0) + ":" + keyData(1)
-    var timeData = 0
+    val keyName = keyData(zero) + ":" + keyData(one)
+    var timeData = zero
     for(value <- values) {
       timeData = value.get()
     }
@@ -37,27 +60,32 @@ class HighestAverageHourReducer extends Reducer[Text,IntWritable,Text,Text] {
     }
   }
 
+  /***
+   * Checks the List for Sequence of Zeros which has to be continuous 6 or more.
+   * @param arrayMins Array[Int]
+   * @return Int
+   */
   def checkForZeros(arrayMins: Array[Int]): Int = {
     try {
-      var zeroCount = 1
-      var countedZero = 0
-      var mins = 0
-      while (mins < arrayMins.length - 1) {
-        if ((arrayMins(mins) == 0) && (arrayMins(mins + 1) == 0 )) {
-          zeroCount += 1
+      var zeroCount = one
+      var countedZero = zero
+      var mins = zero
+      while (mins < arrayMins.length - one) {
+        if ((arrayMins(mins) == zero) && (arrayMins(mins + one) == zero )) {
+          zeroCount += one
         }
         else {
-          if(zeroCount >= 6){
+          if(zeroCount >= idleLimits){
             countedZero += zeroCount
-            zeroCount = 1
+            zeroCount = one
           }
           else {
-            zeroCount = 1
+            zeroCount = one
           }
         }
-        mins += 1
+        mins += one
       }
-      if(zeroCount >= 6) {
+      if(zeroCount >= idleLimits) {
         countedZero += zeroCount
       }
       countedZero
@@ -72,16 +100,22 @@ class HighestAverageHourReducer extends Reducer[Text,IntWritable,Text,Text] {
       }
   }
 
+  /***
+   * Evaluates the time according to 1's and 0's in the List
+   * @param key String
+   * @param durationData ListBuffer[Int]
+   * @return (String, Double)
+   */
   def evaluateTime(key: String, durationData: ListBuffer[Int]): (String, Double) = {
     try {
       val keyName = key.split(":")
       val arrayMins = durationData.toArray
       val zeroCounted = checkForZeros(arrayMins)
-      val overallMins = arrayMins.length * 5
-      val totalMins = overallMins - (zeroCounted * 5).toDouble
+      val overallMins = arrayMins.length * minutes
+      val totalMins = overallMins - (zeroCounted * minutes).toDouble
       val hour: Double = totalMins / 60
       val roundedHours = BigDecimal(hour).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
-      (keyName(0), roundedHours)
+      (keyName(zero), roundedHours)
     }
     catch {
       case ex:Exception =>
@@ -89,6 +123,10 @@ class HighestAverageHourReducer extends Reducer[Text,IntWritable,Text,Text] {
     }
   }
 
+  /**
+   * Before exiting the task writes the data into given context by processing the data as defined
+   * @param context Reducer[Text, IntWritable, Text, Text]#Context
+   */
   override def cleanup(context: Reducer[Text, IntWritable, Text, Text]#Context): Unit = {
     map.foreach {
       x =>
