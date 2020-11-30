adf40e5ddb5ffac314439d8214575dd9dd8f34b4
cheluvesha <cheluvesh.b@gmail.com>
2020-11-30 19:58:41 +0530

    [CHELUVESHA]Added: Driver class

---
+++ src/main/scala/com/highestNoOfIdleHrs/HighestNoOfIdleHours.scala
@@ -0,0 +1,28 @@
+package com.highestNoOfIdleHrs
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.Job
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
+
+object HighestNoOfIdleHours {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new Configuration()
+    val job: Job = new Job(conf, "Highest Idle Hours")
+    job.setJarByClass(classOf[HighestNoOfIdleHoursMapper])
+    job.setMapperClass(classOf[HighestNoOfIdleHoursMapper])
+    job.setReducerClass(classOf[HighestNoOfIdleHoursReducer])
+    job.setMapOutputKeyClass(classOf[Text])
+    job.setMapOutputValueClass(classOf[IntWritable])
+    job.setOutputKeyClass(classOf[Text])
+    job.setOutputValueClass(classOf[Text])
+    FileInputFormat.addInputPath(job, new Path("input"))
+    FileOutputFormat.setOutputPath(job, new Path("output_HighIdleHrs"))
+    job.waitForCompletion(true)
+  }
+}
+
+
